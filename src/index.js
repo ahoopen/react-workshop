c53511747c08f0e8fd437501dfe49585cf568013
ahoopen <auke@frontmen.nl>
2017-03-22 10:09:15 +0100

    lift state exercise

--- src/index.js
+++ src/index.js
@@ -3,22 +3,47 @@ import ReactDOM from 'react-dom';
 import './style.css';
 
 /**
- * Create a counter that increments the count state by 1 when clicked.
+ *
+ * Render the counter button. When the counter reaches 10
+ * show the following message 'You have won!'
+ *
+ * hint: you can pass functions as props
  */
+class Verdict extends Component {
 
-class Counter extends Component {
-
-    state = {};
+    state = {
+        won: false
+    };
 
     render() {
         return (
             <div>
-                <button type="button">increment</button>
+                // your solution
+            </div>
+        );
+    }
+}
+
+class Counter extends Component {
+
+    state = {
+        count: 0
+    };
+
+    increment = () => {
+        this.setState({ count: this.state.count + 1 });
+    };
+
+    render() {
+        return (
+            <div>
+                <button type="button" onClick={this.increment}>increment</button>
+                {this.state.count}
             </div>
         )
     }
 }
 
-ReactDOM.render(<Counter/>,
+ReactDOM.render(<Verdict/>,
     document.getElementById('implementation')
 );
