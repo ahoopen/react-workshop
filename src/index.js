82680645b78fc0b8ac5fb048d2676ddd1b5454b6
ahoopen <auke@frontmen.nl>
2017-03-22 10:04:41 +0100

    JSX exercise

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import './style.css';
+
+const ipsumText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.';
+
+class JSX extends Component {
+
+    render() {
+        return (
+            <div>
+                // your solution here
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<JSX/>,
+    document.getElementById('implementation')
+);
